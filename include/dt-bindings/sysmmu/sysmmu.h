043362f941c18e3e59a6e09410c060a331c9f20d
Flominator <github.flominatorgd@gmail.com>
2026-09-22 16:01:57 +0000

    include: dt-bindings: sysmmu: import Exynos AXI ID macros from 3.18
    The Exynos7870 device trees imported from the lineage-20 3.18 tree
    reference AW()/AR()/NAID in sysmmu pb-info master_axi_id_list
    properties.  Import the macros from the 3.18 bindings next to the
    existing TLB property definitions so the dtsi files preprocess again.
    
    Taken from samsungexynos7870/android_kernel_samsung_exynos7870
    lineage-20 include/dt-bindings/sysmmu/sysmmu.h.
    
    Signed-off-by: Flominator <github.flominatorgd@gmail.com>

--- include/dt-bindings/sysmmu/sysmmu.h
+++ include/dt-bindings/sysmmu/sysmmu.h
@@ -12,6 +12,24 @@
 #define _DT_BINDINGS_EXYNOS_SYSTEM_MMU_H
 
 /*
+ * MAX AXI ID: 65535 (MAX AW_AXI_ID + AW Marker(=0x8000))
+ * NAID: Not Available AXI ID that exceeds MAX AXI ID.
+ * AR: AR AXI ID.
+ * AW: AW AXI ID.
+ */
+#define AXI_ID_MARKER	0x8000
+
+#define NAID		65536
+
+#define AR(x)	(x)
+#define AW(x)	((x) + AXI_ID_MARKER)
+
+#define AXIID_MASK	(AXI_ID_MARKER - 1)
+
+#define is_axi_id(id)		((id) != NAID)
+#define is_ar_axi_id(id)	(!((id) & AXI_ID_MARKER))
+
+/*
  * RESERVED field is used to define properties between DT and driver.
  *
  * CFG[13:12]
